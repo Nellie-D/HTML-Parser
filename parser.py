3ed5e39feb5a4a69a7fb4594bcf49e4d5fbf1d00
Nellie_D <ddroub1@wgu.edu>
2022-09-20 21:57:59 -0400

    What a rough night. I got it to partially work, at least, but I am still having trouble having the computer find the newlines.

--- parser.py
+++ parser.py
@@ -1,16 +1,19 @@
 #This python project is a learning experience 
 
+from asyncio.windows_events import NULL
 from fileinput import lineno
 from html.parser import HTMLParser
 import unicodedata
 import codecs
 import os
+from xml.dom.pulldom import CHARACTERS
 
 
 
-newfilename = input("Enter the name of your file: ")
+newfilename = input("Enter the desired name of your Word document: ")
 
-class parseHTMLData(HTMLParser):
+
+class parseHTMLDataPC(HTMLParser):
     
     def handle_starttag(self, tag, attrs):
       #  print("Encountered a tag:", tag)
@@ -23,38 +26,101 @@ class parseHTMLData(HTMLParser):
 
     def handle_data(self, data):
 
-        #myBookCh = open("C:/Users/danie/myParses/myBookCh.txt", "a", encoding='utf-8')
+       
         newFilePath = os.path.abspath(newfilename)
 
         newSave = open(newFilePath + ".doc", "a")
         
         (line, column) = self.getpos()
         
+       
         if line > 668:
-           # print(data + "\n")
-            if data.endswith("\n"):
-                print("I'm a newline")
-                newSave.writelines("\t" + data + "\n")
+            
+            newSave.writelines("\t" + data + "\n")
+
             
         newSave.close()
 
 
 
+class parseHTMLDataiOS(HTMLParser):
+    
+    #def handle_starttag(self, tag, attrs):
+      #  print("Encountered a tag:", tag)
+        #if (tag) == 'p':
+            #(line1, column) = self.getpos()
+           
+    def handle_data(self, data):
+
+        newFilePath = os.path.abspath(newfilename)
+
+        newSave = open(newFilePath + ".doc", "a")
+        
+        (line, column) = self.getpos()
+        newlineType = '\n'
+        myDataList = []
+        #handlerType = self.handle_starttag(tag="p", attrs 
+        if line > 668:
+            
+            ## my current issue is the intermitent and somewhat random paragraphs found in
+            # occasional pieces of ios data
+            # #fixme    
+            for n in data:
+                #if n != CHARACTERS:
+                    #if n == newlineType:
+                        #print("a newline")
+                    
+                        #newSave.write("\n")   
+            
+                myDataList.append(n)
+                #print(myDataList)
+                
+            if newlineType in myDataList:
+                #returns true
+                newLineIndex = myDataList.index(newlineType)
+
+                print(newLineIndex)
+
+                paragraphStartIndex = newLineIndex + 1
+                myDataList.insert(paragraphStartIndex, "\t")
+                
+                #print(True)
+                
+            else:
+                
+                print(False)
+                    
+            
+            
+           
+            newSave.writelines(myDataList)
+                    
+                        
+            #newSave.writelines("\t" + data + "\n")
+        newSave.flush()
+        newSave.close()
+
 def main(): 
-    folder = input("Enter the folder name: ") + chr(92) + chr(92)
-     
+
+    importType = float(input("Was your document written on PC(1) or on iOS(2) device?"))
     downloadedFile = input("Enter the name of the exported file: ")
-    myStore = folder + downloadedFile
+    
+    myStore = downloadedFile
     downloadPath = os.path.abspath(myStore)
-
-    #f = codecs.open("C:/Users/danie/myParses/chapter8.html", encoding="utf8")
-
     f = codecs.open(downloadPath, encoding="utf8")
 
-    parserprint = parseHTMLData()
 
-    parserprint.feed(f.read())
+    if importType == 1:
+        parserprint = parseHTMLDataPC()
+        parserprint.feed(f.read())
+    elif importType == 2:
+        parserprint = parseHTMLDataiOS()
+        parserprint.feed(f.read())
+    else:
+        print("Invalid entry: " + importType)
+        
 
+    
     parserprint.close()
     f.close()
     
